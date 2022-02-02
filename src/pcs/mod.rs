99a2f5da222d867d46ab3ce06a56dad7c229b063
swasilyev <swasilyev@gmail.com>
2022-02-02 11:39:48 +0300

    aggregate opening claims at the same point

--- src/pcs/mod.rs
+++ src/pcs/mod.rs
@@ -9,6 +9,8 @@ use ark_std::rand::Rng;
 use ark_serialize::*;
 
 use crate::Poly;
+use ark_std::Zero;
+use ark_poly::Polynomial;
 
 
 pub trait CommitmentSpace<F: PrimeField>:
@@ -44,7 +46,8 @@ pub trait VerifierKey: Clone + Debug {
 
 
 pub trait PcsParams<CK, VK> {
-    fn ck(&self) -> CK; //TODO: trim
+    fn ck(&self) -> CK;
+    //TODO: trim
     fn vk(&self) -> VK;
 }
 
@@ -68,15 +71,70 @@ pub trait PCS<F: PrimeField> {
     fn verify(vk: &Self::VK, c: Self::G, x: F, z: F, proof: Self::Proof) -> bool;
 }
 
+/// Represents a claim that f(x) = y, for some f such that CS::commit(f) = c.
+/// Notion of "some f" depends on the soundness properties of the commitment scheme.
+#[derive(Clone)]
+pub struct Claim<F: PrimeField, CS: PCS<F>> {
+    c: CS::G,
+    x: F,
+    y: F,
+}
+
+/// Represents an opening claim together with an alleged proof.
+#[derive(Clone)]
+pub struct Opening<F: PrimeField, CS: PCS<F>> {
+    claim: Claim<F, CS>,
+    proof: CS::Proof,
+}
+
+impl<F: PrimeField, CS: PCS<F>> Claim<F, CS> {
+    fn new(poly: &Poly<F>, at: F, ck: &CS::CK) -> Claim<F, CS> {
+        Claim {
+            c: CS::commit(ck, poly),
+            x: at,
+            y: poly.evaluate(&at),
+        }
+    }
+
+    fn aggregate_at_same_x(claims: &[Claim<F, CS>], rs: &[F]) -> Claim<F, CS> {
+        assert_eq!(claims.len(), rs.len());
+
+        let mut iter_over_xs = claims.iter().map(|cl| cl.x);
+        let same_x = iter_over_xs.next().expect("claims is empty");
+        assert!(iter_over_xs.all(|x| x == same_x));
+
+        let (rcs, rys): (Vec<CS::G>, Vec<F>) = claims.iter().zip(rs.iter())
+            .map(|(cl, &r)| (cl.c.mul(r), r * cl.y)).unzip();
+
+        Claim {
+            c: rcs.into_iter().sum(),
+            x: same_x,
+            y: rys.iter().sum(),
+        }
+    }
+}
+
+fn aggregate_polys<F: PrimeField>(polys: &[Poly<F>], rs: &[F]) -> Poly<F> {
+    assert_eq!(polys.len(), rs.len());
+    polys.iter().zip(rs.iter())
+        .map(|(p, &r)| p * r)
+        .fold(Poly::zero(), |acc, p| acc + p)
+}
 
 #[cfg(test)]
 pub(crate) mod tests {
     use super::*;
 
     use ark_ff::Zero;
-    use ark_poly::Polynomial;
+    use ark_poly::{Polynomial, UVPolynomial};
 
     use crate::Poly;
+    use ark_std::test_rng;
+    use crate::pcs::kzg::KZG;
+
+    pub(crate) type TestCurve = ark_bls12_381::Bls12_381;
+    pub(crate) type TestField = ark_bls12_381::Fr;
+    pub(crate) type TestKzg = KZG::<TestCurve>;
 
     #[derive(Clone)]
     pub struct WrappedPolynomial<F: PrimeField>(pub Poly<F>);
@@ -168,5 +226,34 @@ pub(crate) mod tests {
         fn verify(_vk: &(), c: Self::G, x: F, z: F, _proof: Self::Proof) -> bool {
             c.evaluate(&x) == z
         }
+    }
+
+    fn _test_aggregate_polys<F: PrimeField, CS: PCS<F>>() {
+        let rng = &mut test_rng();
+        let d = 10;
+        let params = CS::setup(d, rng);
+        let (ck, vk) = (params.ck(), params.vk());
+
+        assert!(aggregate_polys::<F>(&[], &[]).is_zero());
+
+        let (polys, rs): (Vec<_>, Vec<_>) = (0..4)
+            .map(|_| (Poly::<F>::rand(d, rng), F::rand(rng)))
+            .unzip();
+        let agg_poly = aggregate_polys(&polys, &rs);
+
+        let same_x = F::rand(rng);
+        let claims_at_same_x = polys.iter().map(|p|
+            Claim::<F, CS>::new(p, same_x, &ck)
+        ).collect::<Vec<_>>();
+        let agg_claim = Claim::aggregate_at_same_x(&claims_at_same_x, &rs);
+
+        let agg_proof = CS::open(&ck, &agg_poly, same_x);
+        assert!(CS::verify(&vk, agg_claim.c, agg_claim.x, agg_claim.y, agg_proof));
+    }
+
+    #[test]
+    fn test_aggregate_polys() {
+        _test_aggregate_polys::<TestField, IdentityCommitment>();
+        _test_aggregate_polys::<TestField, TestKzg>()
     }
 }
