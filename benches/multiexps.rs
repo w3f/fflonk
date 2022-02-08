6297e858feb01ba3d5d12948db574fc7d2e1e0cf
swasilyev <swasilyev@gmail.com>
2022-02-08 16:03:04 +0300

    arkworks' var-base msm vs zcash's small-multiexp

--- benches/multiexps.rs
+++ benches/multiexps.rs
@@ -1,10 +1,11 @@
 use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
 
-use ark_ff::UniformRand;
+use ark_ff::{UniformRand, PrimeField};
 use ark_ec::PairingEngine;
 use ark_std::test_rng;
 
 use fflonk::utils::ec;
+use ark_ec::msm::VariableBase;
 
 
 fn small_multiexp_affine<E: PairingEngine>(c: &mut Criterion) {
@@ -48,9 +49,41 @@ fn small_multiexp_proj<E: PairingEngine>(c: &mut Criterion) {
     group.finish();
 }
 
+fn small_multiexp_vs_msm<E: PairingEngine>(c: &mut Criterion) {
+    let rng = &mut test_rng();
+    let mut group = c.benchmark_group("small-multiexp-vs-msm");
+
+    for n in [10, 20] {
+        let bases = (0..n).map(|_| E::G1Affine::rand(rng)).collect::<Vec<_>>();
+
+        let exps_full = (0..n).map(|_| E::Fr::rand(rng)).collect::<Vec<_>>();
+        let exps_128 = (0..n).map(|_| E::Fr::from(u128::rand(rng))).collect::<Vec<_>>();
+
+        let exps_full_repr = exps_full.iter().map(|exp| exp.into_repr()).collect::<Vec<_>>();
+        let exps_128_repr = exps_128.iter().map(|exp| exp.into_repr()).collect::<Vec<_>>();
+
+
+        group.bench_with_input(BenchmarkId::new("small-multiexp-full", n), &n, |b, _n| {
+            b.iter(|| ec::small_multiexp_affine(&exps_full, &bases))
+        });
+        group.bench_with_input(BenchmarkId::new("var-base-msm-full", n), &n, |b, _n| {
+            b.iter(|| VariableBase::msm(&bases, &exps_full_repr))
+        });
+        group.bench_with_input(BenchmarkId::new("small-multiexp-128", n), &n, |b, _n| {
+            b.iter(|| ec::small_multiexp_affine(&exps_128, &bases))
+        });
+        group.bench_with_input(BenchmarkId::new("var-base-msm-128", n), &n, |b, _n| {
+            b.iter(|| VariableBase::msm(&bases, &exps_128_repr))
+        });
+    }
+
+    group.finish();
+}
+
 
 criterion_group!(benches,
     small_multiexp_affine::<ark_bw6_761::BW6_761>,
     small_multiexp_proj::<ark_bw6_761::BW6_761>,
+    small_multiexp_vs_msm::<ark_bw6_761::BW6_761>,
 );
 criterion_main!(benches);
